e76dbf42cf1eeecd1a5c2b6ad5ef7bc836193a67
Muffin <muffin@muffin.ink>
2024-06-21 12:34:12 -0500

    Revisit removed trademarks message

--- src/components/tw-removed-trademarks/removed-trademarks.jsx
+++ src/components/tw-removed-trademarks/removed-trademarks.jsx
@@ -6,8 +6,9 @@ const RemovedTrademarks = () => (
     <div className={styles.removedTrademarks}>
         <FormattedMessage
             // eslint-disable-next-line max-len
-            defaultMessage="Some items that contained trademarks have been removed."
-            description="Appears in the costume and sprite libraries."
+            defaultMessage="Some items that contained trademarks are no longer listed here."
+            // eslint-disable-next-line max-len
+            description="Appears at the bottom of the builtin 'Choose a Costume' and 'Choose a Sprite' libraries. We are obviously not scanning projects for trademarks, just removing Scratch Cat and some others from the builtin libraries as we do not have permission to use them right now."
             id="tw.removedTrademarks"
         />
     </div>
